668ca9427326ab257d5e603436d45ad66c9d3281
MrTech <ndumchopz@gmail.com>
2025-12-23 03:24:42 +0100

    Add component folder and created the component for the weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Weather from './component/Weather'
 
 const App = () => {
   return (
     <div>
-      <div className="app"></div>
+      <div className="app">
+        <Weather />
+      </div>
     </div>
   )
 }
